b521542aff1fbbe68dae5aff81b85319f574e98a
BrianAKass <kasinator009@gmail.com>
2019-08-23 12:56:47 -0400

    more mongoose 3

--- example/app.js
+++ example/app.js
@@ -1,3 +1,5 @@
+//Commented out code to toggle options. 
+
 
 ////////mongoose way
 const mongoose = require('mongoose');
@@ -6,20 +8,29 @@ mongoose.connect('mongodb://localhost:27017/fruitsdb', {useNewUrlParser: true});
 
 
 const fruitSchema = new mongoose.Schema ({
-    name: String,
-    rating: Number,
-    review: String
+    name: {
+        type:String,
+        //these messages are useful for later.
+        required: [true, "Need a name"],
+    },
+    rating: {
+        //check docs for validations
+        type: Number,
+        min:1,
+        max:10,
+    },
+    review: String,
 });
 
 const Fruit = mongoose.model("Fruit", fruitSchema);
 
 const fruit = new Fruit ({
-    name: "Apple",
-    rating:5,
-    review:"Crunchy",
+    name: "strawberry",
+    rating:10,
+    review:"Strawberries r good",
 });
 
-// fruit.save();
+//fruit.save();
 
 const personSchema = new mongoose.Schema ({
     name: String,
@@ -33,14 +44,15 @@ const person = new Person ({
     age:35,
 });
 
-//person.save()
+// person.save()
+
 const kiwi = new Fruit ({
     name: "Kiwi",
     rating:7,
     review:"tart",
 });
 
-const orange = new Fruit ({
+const apple = new Fruit ({
     name: "Apple",
     rating:8,
     review:"Juicy",
@@ -52,7 +64,7 @@ const banana = new Fruit ({
     review:"food",
 });
 
-// Fruit.insertMany([kiwi,orange,banana],(er)=>{
+// Fruit.insertMany([apple, kiwi, banana],(er)=>{
 //     if (er){
 //         console.log(er);
 //     }else{
@@ -60,7 +72,33 @@ const banana = new Fruit ({
 //     }
 // });
 
-//Looking for items in the database
+
+// Fruit.updateOne({_id:"5d600582aa1b8afcdf0be858"}, {name:"strawberry"},(err)=>{
+//     if (err) {
+//         console.log(err);
+//     }else{
+//         console.log("good job you didn't break it");
+//     }
+// });
+
+// Fruit.deleteOne({_id: "5d5ffca6d79e870178070712"},(err)=>{
+//     if (err) {
+//         console.log(err);
+//     } else {
+//         console.log('Deleted file');
+//     }
+// });
+
+
+Person.deleteMany({name: /John/ },(err)=>{
+    if (err) {
+        console.log(err);
+    } else {
+        console.log('Deleted file');
+    }
+});
+
+//Looking for items in the database. post last to check all updates
 Fruit.find((err,fruits)=>{
     if (err) {
         console.log(err);
